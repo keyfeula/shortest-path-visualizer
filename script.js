eef5681ebcb4c95e98938c9f22bb4c2d823f220e
Key Feula <keyfeula1214@gmail.com>
2024-07-26 13:14:32 -0700

    Add functionality for clear button
    Clear button needs to reset all relevent variables and clear all styling from the grid squares

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const gridDisplay = document.querySelector(".grid-display");
+const clearBtn = document.querySelector(".clear-btn");
 
 let startPointSet = false;
 let endPointSet = false;
 let mouseDown = false;
+let algorithmStarted = false;
+
+let startNode = null;
+let endNode = null;
 
 function gridClickHandler (event) {
     let targetNode = event.target;
     if (!startPointSet) {
         startPointSet = true;
+        startNode = targetNode;
         targetNode.classList.add("start-node");
     }
     else if (startPointSet && !endPointSet) {
         endPointSet = true;
+        endNode = targetNode;
         targetNode.classList.add("end-node");
     }
 }
@@ -34,8 +41,26 @@ function gridMouseUpHandler (event) {
     }
 }
 
+function clearButtonHandler (event) {
+    if (startNode !== null) {
+        startNode.classList.remove("start-node");
+    }
+    if (endNode !== null) {
+        endNode.classList.remove("end-node");
+    }
+    
+    startPointSet = false;
+    endPointSet = false;
+    algorithmStarted = false;
+    startNode = null;
+    endNode = null;
+
+    let gridNodes = document.querySelectorAll(".grid-square");
+    gridNodes.forEach(element => element.classList.remove("obstacle-node"));
+}
+
 gridDisplay.addEventListener("click", gridClickHandler);
 gridDisplay.addEventListener("mousemove", gridDragHandler);
 gridDisplay.addEventListener("mousedown", gridMouseDownHandler);
 gridDisplay.addEventListener("mouseup", gridMouseUpHandler);
-
+clearBtn.addEventListener("click", clearButtonHandler);
